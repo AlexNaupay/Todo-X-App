f01030e23af8acb16fff5dc6dedc8a189eb38098
Alex Naupay <alexnaupay@gmail.com>
2024-11-14 18:29:50 -0500

    ...

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -6,12 +6,12 @@ plugins {
 
 android {
     namespace = "me.alexnaupay.todox"
-    compileSdk = 34
+    compileSdk = 35
 
     defaultConfig {
         applicationId = "me.alexnaupay.todox"
         minSdk = 26
-        targetSdk = 34
+        targetSdk = 35
         versionCode = 1
         versionName = "1.0"
 
